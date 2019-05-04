76f5ae4b794f436c867fe530d80a2a7d381daa84
Peng Liu <peng@tuxera.com>
2019-05-04 23:32:52 +0300

    Improve object detection code to use camera as input source.

--- moose_test/test.py
+++ moose_test/test.py
@@ -3,6 +3,7 @@ import numpy
 import requests
 from pprint import pprint
 import time
+from cv2 import *
 
 TENSORFLOW_SERVING_URL = 'http://localhost:8501/v1/models/ssdlite_mobilenet_v2_coco_2018_05_09:predict'
 
@@ -11,8 +12,14 @@ obj_dict = {17: 'cat', 18: 'dog', 19: 'horse',
             20: 'sheep', 21: 'cow', 22: 'elephant', 23: 'bear', 24: 'zebra', 25: 'giraffe'}
 
 # read image from file
-image = PIL.Image.open("moose_test.jpeg")
-image_np = numpy.array(image)
+#image = PIL.Image.open("moose_test.jpeg")
+#image_np = numpy.array(image)
+
+# read image from usb camera with opencv
+cam = VideoCapture(0)   # 0 -> index of camera
+s, img = cam.read()
+cam.release()
+image_np = cvtColor(img, COLOR_BGR2RGB)
 
 # construct post message to use tensorflow service
 payload = {"instances": [image_np.tolist()]}
@@ -26,6 +33,7 @@ predictions = res.json()['predictions']
 print("Found", len(predictions), "prediction results")
 parsedData = []
 for prediction in predictions:
+    print("numer of detections:", prediction['num_detections'])
     for i in range(int(prediction['num_detections'])):
         obj_class = prediction['detection_classes'][i]
         obj_score = prediction['detection_scores'][i]
@@ -33,6 +41,8 @@ for prediction in predictions:
         if obj_class > 16 and obj_class < 26:
             print(obj_dict[obj_class],
                   obj_score)
-            if obj_score > 0.7:
+            if obj_score > 0.5:
                 print("warning !!! found animal, potentially it is a",
                       obj_dict[obj_class])
+        else:
+            print("found safe object:", obj_class, obj_score)
